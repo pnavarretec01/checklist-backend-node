f13df2fa6b15bb503317592d0551ed975ea6cb66
Pnavarretec <patricionavarrete.c01@gmail.com>
2023-11-15 18:10:09 -0300

    correcciones

--- services/mailer.js
+++ services/mailer.js
@@ -19,7 +19,23 @@ transporter.on("log", (log) => {
   console.log(log);
 });
 
+function formatDateTime(date) {
+  const d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear(),
+    hh = d.getHours(),
+    mm = d.getMinutes();
+
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
+
+  return `${year}-${month}-${day} ${hh}:${mm}`;
+}
+
 async function sendEmail(data, subdivision, idFormulario) {
+  const fechaLocal = formatDateTime(data.fecha);
+
   const mailOptions = {
     from: "Checklist EFE <adminsharepoint@grupoefe.onmicrosoft.com>",
     to: "patricio.navarrete@efe.cl, luis.avalos@efe.cl",
@@ -31,7 +47,7 @@ async function sendEmail(data, subdivision, idFormulario) {
             <h1 style="color: #fff; text-align:center">Checklist EFE</h1>
             <p  style="color: #fff; text-align:center">
                 El usuario <span style="color: #e84393">${data.nombre_supervisor}</span> 
-                ha cerrado un Checklist con fecha de creacion <span style="color: #e84393">${data.fecha}</span>
+                ha cerrado un Checklist con fecha de creacion <span style="color: #e84393">${fechaLocal}</span>
                 correspondiente a la subdivision <span style="color: #e84393">${subdivision}</span>
                 con la observacion "<span style="color: #e84393">${data.observacion_general}</span>".
             </p>
@@ -48,9 +64,9 @@ async function sendEmail(data, subdivision, idFormulario) {
 
   try {
     await transporter.sendMail(mailOptions);
-    console.log("Correo enviado con éxito");
+    //console.log("Correo enviado con éxito");
   } catch (error) {
-    console.error("Error al enviar el correo:", error);
+    //console.error("Error al enviar el correo:", error);
   }
 }
 
